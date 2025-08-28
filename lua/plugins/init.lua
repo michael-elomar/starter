3ae20e5422893948412a3677dfe70281e7f370e6
Michael El Omar <michael.elomar@outlook.com>
2025-08-28 12:15:42 +0200

    configure lsp and formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,9 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
+    cmd = { "ConformInfo" },
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -13,16 +14,63 @@ return {
     end,
   },
 
+  {
+    "FabijanZulj/blame.nvim",
+    lazy = false,
+    config = function()
+      require("blame").setup {}
+    end,
+    opts = {
+      blame_options = { "-w" },
+    },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    config = function()
+      local telescope = require "telescope"
+
+      -- first setup telescope
+      telescope.setup {
+        -- your config
+      }
+
+      -- then load the extension
+      telescope.load_extension "live_grep_args"
+    end,
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "c",
+        "cpp",
+        "python",
+        "bash",
+        "yaml",
+        "groovy",
+        "json",
+        "make",
+        "cmake",
+        "proto",
+      },
+    },
+  },
 }
